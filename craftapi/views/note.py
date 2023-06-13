c7181a5873092c9d4c757402d77a894135f0ab7d
Annaliese Mathews <amesibov043@gmail.com>
2023-06-13 09:56:31 -0500

    added verification so that notes can only be viewed by the project creator

--- craftapi/views/note.py
+++ craftapi/views/note.py
@@ -1,20 +1,24 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from craftapi.models import Note
+from craftapi.models import Note, User, Project
 
 
 class NoteView(ViewSet):
 
     def list(self, request):
         notes = Note.objects.all()
+        user = User.objects.get(pk=request.auth.user.id)
 
         if "project_id" in request.query_params: 
             project_id = int(request.query_params['project_id'])
-            notes = notes.filter(project=project_id)
+            project = Project.objects.get(pk=project_id)
 
-        serializer = NoteSerializer(notes, many=True)
-        return Response(serializer.data)
+            if user.id == project.user_id:
+                notes = notes.filter(project=project_id)
+                serializer = NoteSerializer(notes, many=True)
+                return Response(serializer.data)
+            else: return Response({'message': "This is not you project. Notes can only be viewed by the project creator"})
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta: 
