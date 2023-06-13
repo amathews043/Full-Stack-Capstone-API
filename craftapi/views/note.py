413d4b6446a7473c37ee66bceeca82827a749a58
Annaliese Mathews <amesibov043@gmail.com>
2023-06-13 10:44:22 -0500

    added create to NoteView

--- craftapi/views/note.py
+++ craftapi/views/note.py
@@ -18,7 +18,18 @@ class NoteView(ViewSet):
                 notes = notes.filter(project=project_id)
                 serializer = NoteSerializer(notes, many=True)
                 return Response(serializer.data)
-            else: return Response({'message': "This is not you project. Notes can only be viewed by the project creator"})
+            else: return Response({'message': "This is not you project. Notes can only be viewed by the project creator"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    def create(self, request):
+        project = Project.objects.get(pk=request.data['project'])
+        user = User.objects.get(pk=request.auth.user.id)
+        if user.id == project.user_id:
+            serializer = NoteSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(project=project)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else: return Response({'message': "This is not you project. Notes can only be viewed by the project creator"}, status=status.HTTP_401_UNAUTHORIZED)
+
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta: 
