db522160a3b99bf4480ad8b6d9f5c5933d950906
Annaliese Mathews <amesibov043@gmail.com>
2023-06-22 14:57:32 -0500

    minor change

--- craftapi/admin.py
+++ craftapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Note, Post, Project, Tag, User
 
 # Register your models here.
+admin.site.register(Note)
+admin.site.register(Post)
+admin.site.register(Project)
+admin.site.register(Tag)
+admin.site.register(User)
